265f41b521344ecfd84581bda567d48e2d0fa8bf
Streamtronics <105946463+Streamtronics@users.noreply.github.com>
2022-07-22 08:15:16 +0200

    Created Microchip Studio Project

---
+++ ToyKeeper/spaghetti-monster/anduril/r633_dm1-12-Omnias-42/Debug/anduril.d
@@ -0,0 +1,272 @@
+anduril.d anduril.o: .././anduril.c ../../config-default.h \
+ ../../../../tk.h ../../cfg-noctigon-k9.3-tintramp-fet.h \
+ ../../cfg-noctigon-k9.3-tintramp-nofet.h \
+ ../../../../hwdef-Emisar_D4Sv2-tintramp.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn1634.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../../hank-cfg.h ../../ramp-mode-fsm.h ../../factory-reset-fsm.h \
+ ../../battcheck-mode-fsm.h ../../lockout-mode-fsm.h \
+ ../../strobe-modes-fsm.h ../../load-save-config-fsm.h \
+ ../../../spaghetti-monster.h ../../../../tk-attiny.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\power.h \
+ ../../../fsm-events.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ ../../../fsm-states.h ../../../fsm-adc.h ../../../fsm-wdt.h \
+ ../../../fsm-pcint.h ../../../fsm-standby.h ../../../fsm-ramping.h \
+ ../../../fsm-random.h ../../../fsm-eeprom.h ../../../fsm-misc.h \
+ ../../../fsm-main.h ../../../../tk-delay.h ../../../../tk-attiny.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ ../../../fsm-states.c ../../../fsm-events.c ../../../fsm-adc.c \
+ ../../../fsm-wdt.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h \
+ ../../../fsm-pcint.c ../../../fsm-standby.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
+ ../../../fsm-ramping.c ../../../fsm-random.c ../../../fsm-eeprom.c \
+ ../../../fsm-misc.c ../../../fsm-main.c ../../off-mode.h \
+ ../../ramp-mode.h ../../load-save-config.h ../../config-mode.h \
+ ../../aux-leds.h ../../misc.h ../../sunset-timer.h \
+ ../../version-check-mode.h ../../version.h ../../battcheck-mode.h \
+ ../../beacon-mode.h ../../tempcheck-mode.h ../../lockout-mode.h \
+ ../../momentary-mode.h ../../tint-ramping.h ../../factory-reset.h \
+ ../../strobe-modes.h ../../candle-mode.h ../../off-mode.c \
+ ../../off-mode.h ../../sunset-timer.h ../../ramp-mode.c \
+ ../../ramp-mode.h ../../load-save-config.c ../../load-save-config-fsm.h \
+ ../../load-save-config.h ../../config-mode.c ../../config-mode.h \
+ ../../aux-leds.c ../../aux-leds.h ../../misc.c ../../misc.h \
+ ../../sunset-timer.c ../../version-check-mode.c \
+ ../../version-check-mode.h ../../battcheck-mode.c ../../battcheck-mode.h \
+ ../../beacon-mode.c ../../beacon-mode.h ../../tempcheck-mode.c \
+ ../../tempcheck-mode.h ../../lockout-mode.c ../../lockout-mode.h \
+ ../../momentary-mode.c ../../momentary-mode.h ../../tint-ramping.c \
+ ../../tint-ramping.h ../../factory-reset.c ../../factory-reset.h \
+ ../../strobe-modes.c ../../strobe-modes.h ../../candle-mode.c
+
+../../config-default.h:
+
+../../../../tk.h:
+
+../../cfg-noctigon-k9.3-tintramp-fet.h:
+
+../../cfg-noctigon-k9.3-tintramp-nofet.h:
+
+../../../../hwdef-Emisar_D4Sv2-tintramp.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn1634.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../../hank-cfg.h:
+
+../../ramp-mode-fsm.h:
+
+../../factory-reset-fsm.h:
+
+../../battcheck-mode-fsm.h:
+
+../../lockout-mode-fsm.h:
+
+../../strobe-modes-fsm.h:
+
+../../load-save-config-fsm.h:
+
+../../../spaghetti-monster.h:
+
+../../../../tk-attiny.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\power.h:
+
+../../../fsm-events.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+../../../fsm-states.h:
+
+../../../fsm-adc.h:
+
+../../../fsm-wdt.h:
+
+../../../fsm-pcint.h:
+
+../../../fsm-standby.h:
+
+../../../fsm-ramping.h:
+
+../../../fsm-random.h:
+
+../../../fsm-eeprom.h:
+
+../../../fsm-misc.h:
+
+../../../fsm-main.h:
+
+../../../../tk-delay.h:
+
+../../../../tk-attiny.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+../../../fsm-states.c:
+
+../../../fsm-events.c:
+
+../../../fsm-adc.c:
+
+../../../fsm-wdt.c:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h:
+
+../../../fsm-pcint.c:
+
+../../../fsm-standby.c:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h:
+
+../../../fsm-ramping.c:
+
+../../../fsm-random.c:
+
+../../../fsm-eeprom.c:
+
+../../../fsm-misc.c:
+
+../../../fsm-main.c:
+
+../../off-mode.h:
+
+../../ramp-mode.h:
+
+../../load-save-config.h:
+
+../../config-mode.h:
+
+../../aux-leds.h:
+
+../../misc.h:
+
+../../sunset-timer.h:
+
+../../version-check-mode.h:
+
+../../version.h:
+
+../../battcheck-mode.h:
+
+../../beacon-mode.h:
+
+../../tempcheck-mode.h:
+
+../../lockout-mode.h:
+
+../../momentary-mode.h:
+
+../../tint-ramping.h:
+
+../../factory-reset.h:
+
+../../strobe-modes.h:
+
+../../candle-mode.h:
+
+../../off-mode.c:
+
+../../off-mode.h:
+
+../../sunset-timer.h:
+
+../../ramp-mode.c:
+
+../../ramp-mode.h:
+
+../../load-save-config.c:
+
+../../load-save-config-fsm.h:
+
+../../load-save-config.h:
+
+../../config-mode.c:
+
+../../config-mode.h:
+
+../../aux-leds.c:
+
+../../aux-leds.h:
+
+../../misc.c:
+
+../../misc.h:
+
+../../sunset-timer.c:
+
+../../version-check-mode.c:
+
+../../version-check-mode.h:
+
+../../battcheck-mode.c:
+
+../../battcheck-mode.h:
+
+../../beacon-mode.c:
+
+../../beacon-mode.h:
+
+../../tempcheck-mode.c:
+
+../../tempcheck-mode.h:
+
+../../lockout-mode.c:
+
+../../lockout-mode.h:
+
+../../momentary-mode.c:
+
+../../momentary-mode.h:
+
+../../tint-ramping.c:
+
+../../tint-ramping.h:
+
+../../factory-reset.c:
+
+../../factory-reset.h:
+
+../../strobe-modes.c:
+
+../../strobe-modes.h:
+
+../../candle-mode.c:
